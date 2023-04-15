b4930081ef2e2427035b7da825868cbf0ae48953
Doe John <doejohn@DoenoMacBook-Pro.local>
2023-04-15 15:52:54 +0900

    feat: lexer's read_char defined

---
+++ src/lexer/mod.rs
@@ -0,0 +1,30 @@
+use crate::token;
+
+pub struct Lexer {
+    input: String,
+    position: usize,
+    read_position: usize,
+    ch: Option<char>,
+}
+
+impl Lexer {
+    pub fn new(input: String) -> Lexer {
+        let mut l = Lexer {
+            input,
+            position: 0,
+            read_position: 0,
+            ch: None,
+        };
+        l.read_char();
+        l
+    }
+    fn read_char(&mut self) {
+        if self.read_position >= self.input.len() {
+            self.ch = None;
+        } else {
+            self.ch = self.input.chars().nth(self.read_position)
+        }
+        self.position = self.read_position;
+        self.read_position += 1;
+    }
+}
